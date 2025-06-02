47626984362abd0bc58196b2c181eb99c57c5e23
Mano_douggg <douglas.aleixo2806@gmail.com>
2025-06-01 21:44:16 -0300

    feat: add docker config and db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { DbModule } from './config/db.config.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    DbModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/db.config.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async () => ({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT, 10) || 5432,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: [],
+        autoLoadEntities: true,
+        synchronize: true,
+        logging: true,
+      }),
+    }),
+  ],
+})
+export class DbModule {}
